1c0d2088563d95c49ef4b2f4795d6eaec6ce9052
jeffreyricardokets <93093426+jeffreyricardokets@users.noreply.github.com>
2022-02-19 09:05:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,24 +6,24 @@ __winc_id__ = "ae539110d03e49ea8738fd413ac44ba8"
 __human_name__ = "files"
 
 path = Path(os.path.realpath(__file__)).parent.absolute()
+cache_path = path / 'cache' 
 
 def clean_cache():
-    if os.path.isdir(path):
-        files = os.scandir(path)
+    if os.path.isdir(cache_path):
+        files = os.scandir(cache_path)
         for f in files:
             os.remove(f)
     else:
         os.makedirs(path)
 
-def cache_zip(zip_path, cache_dir):
+def cache_zip(zip_path = path / 'data.zip', cache_dir = path / 'cache'):
     with ZipFile(zip_path, 'r') as zip: 
         zip.extractall(cache_dir)
 
-
 def cached_files():
     list = []
-    for item in os.listdir(path):
-        list.append(os.path.abspath(path + item))
+    for item in os.listdir(cache_path):
+        list.append(os.path.realpath(cache_path / item))
     return list
 
 def find_password(list):
@@ -31,9 +31,5 @@ def find_password(list):
         with open(item, 'r') as file:
             for line in file:
                 if 'password' in line:
-                    return line[line.find(" ") + 1:line.find('</br>')]
+                    return line[line.find("password: ") + len("password: "):line.find('</br>')]
     
-
-cache_zip('/home/jeffrey/Documents/Winc/files/data.zip', '/home/jeffrey/Documents/Winc/files/cache')
-print(cached_files())
-print(find_password(cached_files()))
